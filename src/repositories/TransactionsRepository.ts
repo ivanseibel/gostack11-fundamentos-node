bbca5af308f3ed0d96a95f782730bda01960cd57
Ivan Seibel <ivanseibel@gmail.com>
2020-06-26 11:08:26 -0300

    First commit

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,16 @@ interface Balance {
   total: number;
 }
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
+interface TransactionsList {
+  transactions: Transaction[];
+  balance: Balance;
+}
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -13,16 +23,35 @@ class TransactionsRepository {
     this.transactions = [];
   }
 
-  public all(): Transaction[] {
-    // TODO
+  public all(): TransactionsList {
+    const transactionsList = {
+      transactions: this.transactions,
+      balance: this.getBalance(),
+    };
+    return transactionsList;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const balance: Balance = { income: 0, outcome: 0, total: 0 };
+
+    this.transactions.forEach(item => {
+      if (item.type === 'income') {
+        balance.income += item.value;
+      }
+      if (item.type === 'outcome') {
+        balance.outcome += item.value;
+      }
+    });
+
+    balance.total = balance.income - balance.outcome;
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: Request): Transaction {
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
